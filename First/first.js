45462292b415fc16f5b6d56a637ff1ecfa286233
ilyas78874@gmail.com <ilyas78874@gmail.com>
2024-11-24 12:56:08 +0530

    first & second code

---
+++ First/first.js
@@ -0,0 +1,24 @@
+let name = "Iron man";
+console.log(name);
+let age = 24;             // we can change the variable age 
+age = 25;
+const roll = 12;           // we can't be chanege of variable  or we can say fix value
+console.log(age);
+y = undefined;
+console.log(y);
+isFolow = true;
+
+const student = {
+    name :" ilyas ",
+    age : 24,
+    cgpa : 73
+};
+
+const profile = {
+    name : "Ilyas",
+    isFollow : false,
+    followers : 123,
+    following :123
+};  
+
+console.log(profile);
---
+++ Second/second.js
@@ -0,0 +1,35 @@
+let a = 5;
+let b = 2;
+
+//Arithmatic Operators
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a%b);
+
+console.log(a**b);  // a ka squere
+console.log(++b); 
+
+
+//Unary Opretaors
+console.log(" a = ",a,"b = ",b);
+console.log("a++ =",a++);
+console.log(a);
+
+//Assigment Operators
+console.log("Assigment Operators");
+console.log(a+b);
+
+a += 4;
+console.log(a);
+
+a -= 4;
+console.log(a);
+
+a %= 4;
+console.log(a);
+
+a /= 4;
+console.log(a);
+
+//Comporation operators
