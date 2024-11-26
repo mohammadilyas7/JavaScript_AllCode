b0f34e2c5bdbd0be7c58d63c055d507a5561c0a3
ilyas78874@gmail.com <ilyas78874@gmail.com>
2024-11-26 11:32:56 +0530

    array code

--- Array/array.js
+++ Array/array.js
@@ -17,7 +17,37 @@ let marks = [85,90,55,74,80,66];
 let sum = 0;
 
 for(let mark of marks){
-    sum = sum + mark;
+    // sum = sum + mark;
+    sum += mark;
 }
 console.log("sum of all marks",sum);
 console.log("average marks of subject is : ",sum/marks.length);
+
+
+let items = [250,645,300,900,50];
+console.log(items);
+// let i = 0;
+// for(let item of items){
+//     // console.log('index value ${i}= ',items[i]);
+//     let offer = item / 10;           //after 10 d
+//     items[i] = items[i] - offer;
+//     console.log('after offer ${i} = ',items[i]);
+//     i++;
+// }
+
+for(let i=0; i<items.length; i++){
+    let offer = items[i] / 10;
+    items[i] -= offer;
+}
+console.log('After discout Array ')
+console.log(items);
+
+let number = [250,645,300,900,50];
+// method of this pop() ,push() and toString()
+
+number.push(22,55,77);
+console.log(number);    
+number.pop();
+console.log(number);  
+console.log(number.toString());
+
